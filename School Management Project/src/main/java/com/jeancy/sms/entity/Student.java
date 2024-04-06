c24658dda2d3b1cbe8372bd732a28a30b210ef6d
Yalla Yalla <Yalla Yalla@DESKTOP-7M5ROD4>
2024-04-06 20:01:21 +0200

    Adding age and birthdate fild to the the student entity class.
    Calculating the student age given his birthdate.
    
    adding age and birthdate fild to the edit_student, students and create|_student  html file.
    
    Removing all the getters and setters boiler plate code after adding lombok @Data annotation to the student class

--- School Management Project/src/main/java/com/jeancy/sms/entity/Student.java
+++ School Management Project/src/main/java/com/jeancy/sms/entity/Student.java
@@ -6,7 +6,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.time.LocalDate;
+import lombok.Data;
 
+@Data
 @Entity
 @Table(name="students")
 public class Student {
@@ -17,51 +20,32 @@ public class Student {
     
     @Column(name = "first_name", nullable = false)
     private String firstName;
+    
     @Column(name = "last_name", nullable = false)
+    
     private String lastName;
     @Column(name = "email", nullable = false)
     private String email;
     
-    public Student(String firstName, String lastName, String email) {
+    @Column(name = "birthdate")
+    private LocalDate birthdate;
+    
+    @Column(name = "age")
+    private String age;
+    
+    public Student(String firstName, String lastName, String email, 
+                   LocalDate birthdate) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
+        this.birthdate = birthdate;
     }
 
     public Student() {
     }
     
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
+    public int getAge(){
+        return this.birthdate.until(LocalDate.now()).getYears();
     }
 
     @Override
